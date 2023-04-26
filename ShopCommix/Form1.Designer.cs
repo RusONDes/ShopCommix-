39a5dd65790f946bfe9af6aa186be80a59de2b14
RusONDes <129102059+RusONDes@users.noreply.github.com>
2023-04-26 23:36:08 +0300

    Add files via upload

---
+++ ShopCommix/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ShopCommix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(91, 13);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.Text = "по цене |";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.Text = "по цене v";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(516, 407);
+            this.listBox1.TabIndex = 18;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(539, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 50);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "сохранить ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(539, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 48);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Удалить ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(539, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(539, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(536, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Цена ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(536, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Имя";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.ForestGreen;
+            this.button3.Location = new System.Drawing.Point(539, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 54);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Добавить ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Location = new System.Drawing.Point(538, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 45);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "Добавить свой фаил ";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ShopCommix/Form1.cs
@@ -0,0 +1,181 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ShopCommix
+{
+    public partial class Form1 : Form
+    {
+        string[,] commix = new string[10, 2];
+        public Form1()
+        {
+            InitializeComponent();
+            ReadFormFile();
+            adind = listBox1.SelectedIndex;
+        }
+        int adind;
+        public  void ReadFormFile()
+            
+        {
+            using (StreamReader redear = new StreamReader(@"Products.txt"))
+            {
+                string products;
+                for (int i = 0; !redear.EndOfStream;i++ )
+                {
+                    products = redear.ReadLine();
+                    commix[i, 0] = products.Split(' ')[0];
+                    commix[i, 1] = products.Split(' ')[1];
+                }
+            }
+            refresh();
+        }
+        private void refresh()
+        {
+            listBox1.Items.Clear();
+            for (int i = 0;i < commix.GetLength(0);i++)
+            {
+                listBox1.Items.Add($"название {commix[i,0]} ; Цена:  {commix[i,1]}");
+            }
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            string tempOne;
+            string tempTwo;
+            for (int i = 0; i < commix.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < commix.GetLength(0) ; j++)
+                {
+                    if (int.Parse(commix[i, 1]) < int.Parse(commix[j, 1]))
+                    {
+                        tempOne = commix[i, 0];
+                        tempTwo = commix[i, 1];
+                        commix[i, 0] = commix[j, 0];
+                        commix[i, 1] = commix[j, 1];
+                        commix[j, 0] = tempOne;
+                        commix[j, 1] = tempTwo;
+                    }
+                }
+            }
+            refresh();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "куда вы хотиле сохранить фаил ";
+            saveFileDialog1.Filter = "все фаилы|*.*|текстовые фаилы |*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string patc = saveFileDialog1.FileName;
+                FileStream dr = new FileStream(patc, FileMode.Create, FileAccess.Write);
+                StreamWriter rf = new StreamWriter(dr);
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    rf.WriteLine(listBox1.Items[i]);
+                }
+                rf.Close();
+                dr.Close();
+            }
+            else
+            {
+                MessageBox.Show("с фаиллом что-то случилась ");
+            }
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (adind != -1)
+            {
+                listBox1.Items.RemoveAt(adind);
+             
+            }
+            else
+            {
+                MessageBox.Show("вы не выбрали элемент ");
+            }
+            
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            adind = listBox1.SelectedIndex;
+            textBox1.Text = commix[adind, 0];
+            textBox2.Text = commix[adind, 1];
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            string tempOne;
+            string tempTwo;
+            for (int i = 0; i < commix.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < commix.GetLength(0); j++)
+                {
+                    if (int.Parse(commix[i, 1]) > int.Parse(commix[j, 1]))
+                    {
+                        tempOne = commix[i, 0];
+                        tempTwo = commix[i, 1];
+                        commix[i, 0] = commix[j, 0];
+                        commix[i, 1] = commix[j, 1];
+                        commix[j, 0] = tempOne;
+                        commix[j, 1] = tempTwo;
+                    }
+                }
+            }
+            refresh();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text)|| !string.IsNullOrEmpty(textBox2.Text))
+            {
+                commix[listBox1.SelectedIndex, 0] = textBox1.Text;
+                commix[listBox1.SelectedIndex, 1] = textBox2.Text;
+                refresh();
+            }
+            else if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("ВВИДИТЕ ИМЯ ");
+            }
+            else if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("ВВИДИТЕ ЦЕНУ  ");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "куда вы хотиле сохранить фаил ";
+            openFileDialog1.Filter = "все фаилы|*.*|текстовые фаилы |*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream file = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader reder = new StreamReader(file);
+                while (!reder.EndOfStream)
+                {
+                    listBox1.Items.Add (reder.ReadLine());
+                }
+                refresh();
+                reder.Close();
+                file.Close();
+            }
+        }
+    }
+    
+}
